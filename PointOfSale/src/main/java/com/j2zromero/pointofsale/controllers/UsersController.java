620b7b9309e5215e7d0dd07d27869e9b7dae4705
J2ZROMERO <jose-zromero@hotmail.com>
2024-01-25 22:37:31 -0600

    Add methods to verify data inputs in users

--- PointOfSale/src/main/java/com/j2zromero/pointofsale/controllers/UsersController.java
+++ PointOfSale/src/main/java/com/j2zromero/pointofsale/controllers/UsersController.java
@@ -1,30 +1,47 @@
 package com.j2zromero.pointofsale.controllers;
 
+import com.j2zromero.pointofsale.utils.GenericValue;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class UsersController extends Application {
+    @FXML // this loader will inyect all of the values in this controller
+    public TextField txt_id;
+    public TextField txt_rol;
+    public TextField txt_nombre;
+    public TextField txt_apellido;
+    public TextField txt_password;
+    public ChoiceBox roleChoiceBox;
 
-    @FXML
-    private Button add;
 
-    @FXML
-    private void eventKey(KeyEvent e){
-        System.out.println("algo algo");
+    public void getData(ActionEvent e){
+
+
     }
 
-    @FXML
-    private void eventAction(ActionEvent e){
-
+    public Object getDataInputs(Node inputElement) {
+        if (inputElement instanceof TextField) {
+            return (String)((TextField) inputElement).getText();
+        } else if (inputElement instanceof ChoiceBox) {
+            return (String)((ChoiceBox<?>) inputElement).getValue();
+        }
+        // Handle other cases or return a default value
+        return null;
     }
+
+
+
     @Override
     public void start(Stage stage) {
         try {
